4cef1cfd489d63da1b4824cc55669f425f0ddd51
dzzoja <134314040+dzzoja@users.noreply.github.com>
2024-03-14 11:42:39 +0100

    Delete zd1.py

--- zd1.py
+++
@@ -1,7 +0,0 @@
-def total_euro(radni_sati, eur_h):
-    print("Ukupno: ", radni_sati * eur_h) #def funkcije i ispis zarade
-
-radni_sati = int(input("Radni sati: "))
-eur_h = float(input("Satnica(€/h): ")) 
-
-total_euro(radni_sati, eur_h) #poziv funkcije
